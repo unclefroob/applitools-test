b9de26c3226615a8fe6103dbc223a5d56ea67aac
Ryan Kwan <ryan@kwans.co.nz>
2019-12-20 16:19:28 +1300

    add select item automation

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -2,8 +2,10 @@ const expect = require("chai").expect;
 
 describe("automate customer journey on ebay web page", () => {
   it("browse to ebay web page", () => {
+    //defaulting to AU site
     browser.url("https://www.ebay.com.au/");
-    //check title string contails 'eBay'
+
+    //check title string contains 'eBay'
     const title = browser.getTitle();
     expect(title)
       .to.be.a("string")
@@ -16,9 +18,20 @@ describe("automate customer journey on ebay web page", () => {
     browser.keys("iPhone");
     $("#gh-btn").click();
 
+    //check results exist - srp-river-results-message1 ID only appears when suggested search happened instead
     expect(
       $("#srp-river-results-message1").isExisting() ||
         $(".srp-controls__count-heading span").getText() === "0"
     ).eq(false);
+  });
+
+  it("select item in results", () => {
+    //get name of item before clicking
+    const itemTitle = $("#srp-river-results ul li .s-item__title").getText();
+    $("#srp-river-results ul li .s-item__title").click();
+
+    //check new page title is name of item
+    const pageTitle = browser.getTitle();
+    expect(pageTitle).includes(itemTitle);
   });
 });
